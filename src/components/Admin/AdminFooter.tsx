d2180f7152712fe5a4687b725c9301b8e8e96067
chamber613 <chamber61390@gmail.com>
2022-10-20 17:01:01 +0300

    Update

---
+++ src/components/Admin/AdminFooter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminFooter = () => {
+
+    return (
+        <div className="d-flex bg-black justify-content-between px-5 pt-2">
+            <p><span>FoodShop | Admin Panel</span></p>
+            <p className="opacity-75">Copyright &copy; 2022 By <span className='fw-bold'>Chamber613</span></p>
+        </div>
+    )
+}
+
+export default AdminFooter
